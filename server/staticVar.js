b81e99e12894ecca1048c0fdf0810d2b3f0d6fe8
Karen <mooshi21824@gmail.com>
2022-12-28 10:31:40 +0800

    改成抓取環境變數

---
+++ server/staticVar.js
@@ -0,0 +1,7 @@
+exports.AppID = process.env.APP_ID
+exports.AppKey = process.env.APP_KEY
+
+exports.CLIENT_ID = process.env.CLIENT_ID
+exports.CLIENT_SECRET = process.env.CLIENT_SECRET
+
+exports.ENCODE_KEY = process.env.ENCODE_KEY
